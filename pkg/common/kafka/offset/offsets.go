2e8c28c0efb1296b23bc3a48b29406dbdbd493a9
Ali Ok <aliok@redhat.com>
2021-10-11 11:45:47 +0300

    Kafka ch event loss openshift 026 (#389)
    * Move initOffsets functionality to a common package (#887)
    
    Co-authored-by: Ali Ok <aliok@redhat.com>
    
    * Fix Kafka channel event loss during subscription becoming ready (#928)
    
    Co-authored-by: Knative Prow Robot <knative-prow-robot@google.com>

--- pkg/common/kafka/offset/offsets.go
+++ pkg/common/kafka/offset/offsets.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package client
+package offset
 
 import (
 	"context"
@@ -28,33 +28,19 @@ import (
 )
 
 // We want to make sure that ALL consumer group offsets are set before marking
-// the source as ready, to avoid "losing" events in case the consumer group session
+// the resource as ready, to avoid "losing" events in case the consumer group session
 // is closed before at least one message is consumed from ALL partitions.
 // Without InitOffsets, an event sent to a partition with an uninitialized offset
 // will not be forwarded when the session is closed (or a rebalancing is in progress).
-func InitOffsets(ctx context.Context, kafkaClient sarama.Client, topics []string, consumerGroup string) (int32, error) {
-	totalPartitions := 0
+func InitOffsets(ctx context.Context, kafkaClient sarama.Client, kafkaAdminClient sarama.ClusterAdmin, topics []string, consumerGroup string) (int32, error) {
 	offsetManager, err := sarama.NewOffsetManagerFromClient(consumerGroup, kafkaClient)
 	if err != nil {
 		return -1, err
 	}
 
-	kafkaAdminClient, err := sarama.NewClusterAdminFromClient(kafkaClient)
+	totalPartitions, topicPartitions, err := retrieveAllPartitions(topics, kafkaClient)
 	if err != nil {
-		return -1, fmt.Errorf("failed to create a Kafka admin client: %w", err)
-	}
-	defer kafkaAdminClient.Close()
-
-	// Retrieve all partitions
-	topicPartitions := make(map[string][]int32)
-	for _, topic := range topics {
-		partitions, err := kafkaClient.Partitions(topic)
-		totalPartitions += len(partitions)
-		if err != nil {
-			return -1, fmt.Errorf("failed to get partitions for topic %s: %w", topic, err)
-		}
-
-		topicPartitions[topic] = partitions
+		return -1, err
 	}
 
 	// Fetch topic offsets
@@ -103,7 +89,51 @@ func InitOffsets(ctx context.Context, kafkaClient sarama.Client, topics []string
 		logging.FromContext(ctx).Infow("consumer group offsets committed", zap.String("consumergroup", consumerGroup))
 	}
 
-	// At this stage the KafkaSource instance is considered Ready
+	// At this stage the resource is considered Ready
 	return int32(totalPartitions), nil
 
+}
+
+func CheckIfAllOffsetsInitialized(kafkaClient sarama.Client, kafkaAdminClient sarama.ClusterAdmin, topics []string, consumerGroup string) (bool, error) {
+	_, topicPartitions, err := retrieveAllPartitions(topics, kafkaClient)
+	if err != nil {
+		return false, err
+	}
+
+	// Look for uninitialized offset (-1)
+	offsets, err := kafkaAdminClient.ListConsumerGroupOffsets(consumerGroup, topicPartitions)
+	if err != nil {
+		return false, err
+	}
+
+	for _, partitions := range offsets.Blocks {
+		for _, block := range partitions {
+			if block.Offset == -1 { // not initialized?
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
+}
+
+func retrieveAllPartitions(topics []string, kafkaClient sarama.Client) (int, map[string][]int32, error) {
+	totalPartitions := 0
+
+	// Retrieve all partitions
+	topicPartitions := make(map[string][]int32)
+	for _, topic := range topics {
+		partitions, err := kafkaClient.Partitions(topic)
+		totalPartitions += len(partitions)
+		if err != nil {
+			return -1, nil, fmt.Errorf("failed to get partitions for topic %s: %w", topic, err)
+		}
+
+		// return a copy of the partitions array in the map
+		// Sarama is caching this array and we don't want nobody to mess with it
+		clone := make([]int32, len(partitions))
+		copy(clone, partitions)
+		topicPartitions[topic] = clone
+	}
+	return totalPartitions, topicPartitions, nil
 }
