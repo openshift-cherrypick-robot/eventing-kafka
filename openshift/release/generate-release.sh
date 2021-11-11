2b2d2f2f432da7427a7473108ff830d060d55ba6
afield <afield@redhat.com>
2021-11-11 05:45:28 -0500

    Add openshift specific files.

---
+++ openshift/release/generate-release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source $(dirname $0)/resolve.sh
+
+release=$1
+
+source_output_file="openshift/release/knative-eventing-kafka-source-ci.yaml"
+source_postinstall_output_file="openshift/release/knative-eventing-kafka-source-postinstall-ci.yaml"
+channel_output_file="openshift/release/knative-eventing-kafka-channel-ci.yaml"
+channel_postinstall_file="openshift/release/knative-eventing-kafka-channel-postinstall-ci.yaml"
+distributed_channel_output_file="openshift/release/knative-eventing-kafka-distributed-channel-ci.yaml"
+
+if [ "$release" == "ci" ]; then
+    image_prefix="registry.ci.openshift.org/openshift/knative-nightly:knative-eventing-kafka-"
+    tag=""
+else
+    image_prefix="registry.ci.openshift.org/openshift/knative-${release}:knative-eventing-kafka-"
+    tag=""
+fi
+
+# the source parts
+resolve_resources config/source/single $source_output_file $image_prefix $tag
+resolve_resources config/source/post-install $source_postinstall_output_file $image_prefix $tag
+
+# the channel parts
+resolve_resources config/channel/consolidated $channel_output_file $image_prefix $tag
+resolve_resources config/channel/post-install $channel_postinstall_file $image_prefix $tag
+
+# the distributed channel parts
+resolve_resources config/channel/distributed $distributed_channel_output_file $image_prefix $tag
