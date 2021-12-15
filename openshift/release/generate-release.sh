3dc6b642aaadc7b9aca74ed7aab4e90b0b6b7fe3
Serverless QE Robot <ocf-qe@redhat.com>
2021-12-14 19:34:14 -0500

    Add openshift specific files.

---
+++ openshift/release/generate-release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source $(dirname $0)/resolve.sh
+
+release=$1
+
+source_output_file="openshift/release/knative-eventing-kafka-source-ci.yaml"
+source_postinstall_output_file="openshift/release/knative-eventing-kafka-source-postinstall-ci.yaml"
+channel_output_file="openshift/release/knative-eventing-kafka-channel-ci.yaml"
+channel_postinstall_file="openshift/release/knative-eventing-kafka-channel-postinstall-ci.yaml"
+
+if [ "$release" == "ci" ]; then
+    image_prefix="registry.ci.openshift.org/openshift/knative-nightly:knative-eventing-kafka-"
+    tag=""
+else
+    image_prefix="registry.ci.openshift.org/openshift/knative-${release}:knative-eventing-kafka-"
+    tag=""
+fi
+
+# the source parts
+resolve_resources config/source/single $source_output_file $image_prefix $tag
+resolve_resources config/source/post-install $source_postinstall_output_file $image_prefix $tag
+
+# the channel parts
+resolve_resources config/channel/consolidated $channel_output_file $image_prefix $tag
+resolve_resources config/channel/post-install $channel_postinstall_file $image_prefix $tag
