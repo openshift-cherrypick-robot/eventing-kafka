ce63500d396dbb7636591cd8b5661486699b49a4
Ahmed Abdalla <aabdelre@redhat.com>
2022-02-03 12:18:13 +0100

    Fix typos
    Signed-off-by: Ahmed Abdalla <aabdelre@redhat.com>

--- openshift/so-forward-compat-tests.sh
+++ openshift/so-forward-compat-tests.sh
@@ -11,10 +11,10 @@ export TEST_IMAGE_TEMPLATE="${EVENTING_KAFKA_TEST_IMAGE_TEMPLATE}"
 # Check if the same version of eventing is available via the serverless operator
 so_branch="$(find_matching_so_release_branch)"
 if [[ "$so_branch" == "0" ]]; then
-  # Only run e2e tests in case we serverless operator doesn't have a matching
+  # Only run e2e tests in case the serverless operator doesn't have a matching
   # eventing version.
   # This assumes that the version provided by SO is an older eventing version,
-  # thus a forward compatability test is needed.
+  # thus a forward compatibility test is needed.
 
   # Don't fall back to midstream knative and use serverless operator knative
   sh "$(dirname "$0")/e2e-tests.sh" false
